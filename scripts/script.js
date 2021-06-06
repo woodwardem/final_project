06389144bd8692f4fa1b50295465af7af3501c4f
McCall Woodward <woodwardem@g.crookcoountyschools.org>
2021-06-06 15:18:18 -0700

    code

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,18 @@
 //console.log('hello world');
+
+function myFunction() {
+if (clicks !== 25 ) {
+    let random = Math.floor(Math.random())
+}
+let random = Math.floor(Math.random())
+
+}
+
+
+var text = document.createElement('Input');
+text.setAttribute("type", "text");
+
+var button = document.createElement('button');
+var b = document.getElementById('button');
+
+
