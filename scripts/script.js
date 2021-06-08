0dcb45b690f7f07fd761d88f6ff5322771d19bc8
woodwardem <woodwardem@g.crookcountyschools.org>
2021-06-08 09:17:18 -0700

    code

--- scripts/script.js
+++ scripts/script.js
@@ -4,14 +4,29 @@
 
 let answers = ['not today', 'i do not see it in your future', 'absolutely', 'almost positive', 'you will fail', 'no'];
  function respond1() {
-     
+    let respond1 = document.getElementById('button');
+    respond1.addEventListener ('click', answers.button)
+    let article = document.createElement('article');
+    respond1.appendChild(article);
  }
 
+  respond1();
 
-var text = document.createElement('Input');
-text.setAttribute("type", "text");
+  for (let i=0; i<6; i++) {
+      let random = Math.floor(Math.random()* answers.length -1);
 
-var button = document.createElement('button');
-var b = document.getElementById('button');
+       let item = answers.shift();
+       answers.splice(answers.length -1, 0, item);
+  
+  }
+  
+   answers.appendChild(article);
+  
+  var text = document.createElement('Input');
+  text.setAttribute("type", "text");
+
+  var button = document.createElement('button');
+  var button = document.getElementById('button');
 
 
+  var button = respond1();
