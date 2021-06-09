3ac2af35717ab62ff661e576958e2bbc25ee3bd8
woodwardem <woodwardem@g.crookcountyschools.org>
2021-06-09 12:43:55 -0700

    code

--- scripts/script.js
+++ scripts/script.js
@@ -6,18 +6,17 @@ let answers = ['not today', 'i do not see it in your future', 'absolutely', 'alm
  function respond1() {
 
     let respond1 = document.getElementById('button');
-    respond1.addEventListener ('click', answers.random);
+    respond1.addEventListener ('click', answers);
     let article = document.getElementById('touch');
      
   
 
    for (let i=0; i<6; i++) {
      
-      let random = Math.floor(Math.random()* answers.length -1);
-       var answers = random;
-       let item = answers.shift();
-       answers.splice(answers.length -1, 0, item);
-       answers.appendChild(article);
+      let random = Math.floor(Math.random()* answers -1);
+       
+       
+    
   }
 }
 respond1();
